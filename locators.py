15201f817acdbb040f5cf4b12db910ff5cf293d2
dimonbrom <stak-1@yandex.ru>
2022-01-10 19:27:50 +0400

    локаторы (селекторы) для тестов

--- locators.py
+++ locators.py
@@ -8,3 +8,5 @@ class  LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM =(By.CSS_SELECTOR, "#register_form")
     
+class ProductPageLocators():
+    ADD = (BY. CSS_SELECTOR, "[value = 'Добавить в корзину']"
