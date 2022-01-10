cf8505b3f098fae931deaa977b393fd6102fbf21
dimonbrom <stak-1@yandex.ru>
2022-01-10 19:37:26 +0400

    локаторы (селекторы) для тестов

--- locators.py
+++ locators.py
@@ -4,9 +4,15 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class  LoginPageLocators():
+class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM =(By.CSS_SELECTOR, "#register_form")
     
+
 class ProductPageLocators():
-    ADD = (BY. CSS_SELECTOR, "[value = 'Добавить в корзину']"
+    ADD = (By. CSS_SELECTOR, "#add_to_basket_form>.btn")
+    NAME_PRODUCT=(By. CSS_SELECTOR, ".product_main>h1")
+    NAME_ALERT = (By. CSS_SELECTOR, "div.alertinner strong")
+    PRODUCT_ADDED =(By. CSS_SELECTOR, "#messages>.alert>div.alertinner")
+    PRICE_BOOK = (By. CSS_SELECTOR, ".product_main>.price_color")
+    PRICE_BASKET = (By. CSS_SELECTOR, ".alertinner>p>strong")
