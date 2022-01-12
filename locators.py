2d3571f63ac927c9f1ee246f3fa929ef3ebd97f0
dimonbrom <stak-1@yandex.ru>
2022-01-12 15:54:27 +0400

    селекторы для автотестов

--- locators.py
+++ locators.py
@@ -17,8 +17,12 @@ class ProductPageLocators():
     PRICE_BOOK = (By. CSS_SELECTOR, ".product_main>.price_color")
     PRICE_BASKET = (By. CSS_SELECTOR, ".alertinner>p>strong")
     SUCCESS_MESSAGE = (By. CSS_SELECTOR, "#messages>.alert>div.alertinner")
-
+    BASKET_CONTENT = (By. CSS_SELECTOR, ".basket_summary")
+    TEXT_BASKET = (By. CSS_SELECTOR, "#content_inner>p")
+    
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BUTTON_BASKET = (By.CSS_SELECTOR, ".btn-group>.btn")
+    
