fb8bf15805dd472270a47aafc37c0e3dc8fb2efc
dimonbrom <stak-1@yandex.ru>
2022-01-12 15:59:20 +0400

    тесты кейсы

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,8 @@
 import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+from .pages.product_page import ProductPage
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -38,3 +40,13 @@ def test_correct_URL_guest(browser):
     page = LoginPage (browser, link)
     page.open()
     page.should_be_login_url()
+
+@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/"
+    page = BasketPage (browser, link)
+    page.open()
+    page.go_to_basket()
+    page.cheking_that_empty_basket()
+    page.checking_that_there_text()
+    
